ebc4903c13dfd6aa022d96149f967d42f7c59346
pmoralesm355 <pablo.moralesm355@gmail.com>
2024-08-27 22:53:37 -0600

    testtt

--- Hotel/frontend/sistema_hoteles/src/components/HomePage.jsx
+++ Hotel/frontend/sistema_hoteles/src/components/HomePage.jsx
@@ -194,7 +194,7 @@ const applyAdditionalFilters = () => {
         const filteredRooms = await response.json();
   
   
-      // trasn1233 las habitaciones filtradas en una estructura de hoteles con habitaciones
+      // trasn1233 las habitdsadaciones filtradas en una estructura de hoteles con habitaciones
       const hotelsMap = {};
       for (const room of filteredRooms) {
         if (!hotelsMap[room.id_hotel]) {
