52358d48584f51e9c256d73c7997310584193114
Pablo Morales <pablo.moralesm355@gmail.com>
2024-09-06 17:55:26 -0600

    Añadir archivo FAQs

--- Hotel/frontend/sistema_hoteles/src/components/FAQs.jsx
+++ Hotel/frontend/sistema_hoteles/src/components/FAQs.jsx
@@ -17,7 +17,7 @@ function FAQs() {
     fetchSeccionesGenerales();
   }, []);
 
-    // Fetch FAQsdasdasd
+    // Fetch FAQsdasdasd312312321
     async function fetchFaqs() {
         const response = await fetch('http://localhost:8090/faqs');
         const data = await response.json();
