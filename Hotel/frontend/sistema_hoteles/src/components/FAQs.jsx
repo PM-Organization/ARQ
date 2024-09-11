a3300fc8294358295e899d2b1ea864cf07ae9088
pmoralesm355 <82289481+pmoralesm355@users.noreply.github.com>
2024-09-11 02:29:43 -0600

    Update FAQs.jsx

--- Hotel/frontend/sistema_hoteles/src/components/FAQs.jsx
+++ Hotel/frontend/sistema_hoteles/src/components/FAQs.jsx
@@ -17,7 +17,7 @@ function FAQs() {
     fetchSeccionesGenerales();
   }, []);
 
-    // Fetch FAQsdasdasd312313
+    // Fetch FAQsdasdasd312313DE2132312321
     async function fetchFaqs() {
         const response = await fetch('http://localhost:8090/faqs');
         const data = await response.json();
