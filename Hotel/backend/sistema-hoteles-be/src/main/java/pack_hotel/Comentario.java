066cc0b4d1f85e00a3cbf3a7e04f147053885ecc
Pablo Morales <pablo.moralesm355@gmail.com>
2024-10-24 19:02:12 -0600

    JENKINS

--- Hotel/backend/sistema-hoteles-be/src/main/java/pack_hotel/Comentario.java
+++ Hotel/backend/sistema-hoteles-be/src/main/java/pack_hotel/Comentario.java
@@ -97,11 +97,6 @@ public class Comentario {
     }
 
     
-    public void checkComentario() {
-        if (true) { 
-            System.out.println("Esta condición siempre es verdadera");
-        }
-    }
 
     // Equals and hashCode methods
 
